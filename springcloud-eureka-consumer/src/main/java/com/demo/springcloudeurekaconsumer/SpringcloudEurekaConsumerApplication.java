fb910df9a4da1ea1d8138da5c58a7d824be0c983
liumingquan <liumingquan@021.com>
2019-10-14 17:41:44 +0800

    Initial commit

---
+++ springcloud-eureka-consumer/src/main/java/com/demo/springcloudeurekaconsumer/SpringcloudEurekaConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.demo.springcloudeurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class SpringcloudEurekaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaConsumerApplication.class, args);
+	}
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
